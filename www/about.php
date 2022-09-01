c56f2058bd7249105f87578c5e26db3cb55614b3
nanthachaithanks <645162010011@dpu.ac.th>
2022-09-01 18:34:24 +0700

    first commit

---
+++ www/about.php
@@ -0,0 +1,10 @@
+<html>
+        <head>
+                <title>Lab4</title>
+        </head>
+        <body
+        <p>Name : นันทชัย หมื่นหนู</p><br/>
+        <p>ID : 645162010011</p><br/>
+        <a href="index.php">Back</a>
+        </body>
+</html>
